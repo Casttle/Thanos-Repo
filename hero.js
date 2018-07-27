759a29a31accdf98a0b5883327361882b883d488
Casttle <cberry1222@gmail.com>
2018-07-26 21:14:07 -0500

    added normal and movement

--- hero.js
+++ hero.js
@@ -20,4 +20,37 @@ $(".grow-button").on("click", function() {
     $(thanos).animate({ height: "650px",width: "500px",top:"-150px" },5000);
 
   });
+
+  $(".normal-button").on("click", function() {
+    $(thanos).animate({ height: "416px", width: "305px", top: "50px"});
+  });
+
+   // Keyboard move controls
+   $(document).keyup(function(e) {
+    switch (e.which) {
+
+    // Move Buttons (Keyboard Down)
+    case 40:
+      $(thanos).animate({ top: "+=200px" }, "normal");
+      break;
+
+      // Move Buttons (Keyboard Right)
+    case 39:
+      $(thanos).animate({ left: "+=200px" }, "normal");
+      break;
+
+      // Move Buttons (Keyboard Up)
+    case 38:
+      $(thanos).animate({ top: "-=200px" }, "normal");
+      break;
+
+      // Move Buttons (Keyboard Left)
+    case 37:
+      $(thanos).animate({ left: "-=200px" }, "normal");
+      break;
+
+    default:
+      break;
+    }
+  });
 });
